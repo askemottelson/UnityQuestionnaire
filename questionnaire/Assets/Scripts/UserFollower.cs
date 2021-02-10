a81f6ad28f93c95214967bf886197f9aee1e9e1c
Aske Mottelson <aske@mottelson.dk>
2021-02-10 21:55:38 +0100

    make easier

--- questionnaire/Assets/Scripts/UserFollower.cs
+++ questionnaire/Assets/Scripts/UserFollower.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class UserFollower : MonoBehaviour {
     public GameObject User;
+    private float step = 0.5f;
+    private float e = 1.0f * 0.0001f;
 
     void Start() {
         
     }
 
+    void OnEnable()
+    {
+        // enable move
+        step = 0.05f;
+    }
+
+    void decreaseStep()
+    {
+        step = step - e;
+    }
+
     void Update() {
+        decreaseStep();
+
         var newPos = User.transform.position + User.transform.forward * 0.4f;
-        transform.position = Vector3.Lerp(transform.position, newPos, 0.05f);
+        transform.position = Vector3.Lerp(transform.position, newPos, step);
 
         var toUser = User.transform.position - transform.position;
         //toUser = Vector3.ProjectOnPlane(toUser, Vector3.up).normalized;
